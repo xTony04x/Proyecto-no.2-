1cfbfd51feb36ac5d1b489387b9e074b5b277f41
xTony0x <jabelteton@correo.utl.edu.gt>
2022-11-04 17:17:21 -0600

    creando lista

--- Proyecto_2_AB_BG/Proyecto_2_AB_BG/Proyecto_2_AB_BG.cpp
+++ Proyecto_2_AB_BG/Proyecto_2_AB_BG/Proyecto_2_AB_BG.cpp
@@ -1,8 +1,31 @@
 #include "pch.h"
-
+#include <iostream>
+#include <conio.h>
+#include <stdlib.h>
+#include <string>
 using namespace System;
+using namespace std;
 
-int main(array<System::String ^> ^args)
+struct nodo
 {
-    return 0;
+    string dato;
+    nodo* next;
+};
+void insertarlista(nodo *&, string);
+int main()
+{
+    string n;
+    nodo* lista = NULL;
+    cout << "ingrese un texto: "; cin >> n;
+    insertarlista(lista, n);
+
+}
+void insertarlista(nodo*& lista, string n) {
+    nodo* newnodo = new nodo();
+    newnodo->dato = n;
+
+    nodo *aux1 = lista;
+    nodo* aux2;
+    
+    newnodo->next = aux1;
 }
