b1e51652e9e4c974d12c163b8598cbba43936c12
xTony0x <jabelteton@correo.utl.edu.gt>
2022-11-04 18:01:08 -0600

    creacion de menu y primer lista

--- Proyecto_2_AB_BG/Proyecto_2_AB_BG/Proyecto_2_AB_BG.cpp
+++ Proyecto_2_AB_BG/Proyecto_2_AB_BG/Proyecto_2_AB_BG.cpp
@@ -15,10 +15,49 @@ void insertarlista(nodo *&, string);
 int main()
 {
     string n;
+    int num,menu,fila;
+    int fila1 = 1, fila2 = 2 , fila3 = 3, fila4 = 4;
     nodo* lista = NULL;
-    cout << "ingrese un texto: "; cin >> n;
-    insertarlista(lista, n);
 
+    cout << "MENU: \n (1)-Ingresar Producto. \n (2)-Sacar Producto \n (3)-Salir \n"; cin >> menu;
+
+        switch(menu)
+        {
+        case 1:
+            cout << "\n En que columna desea ingresar los productos? \n (1)(2)(3)(4)"; cin >> fila;
+            if (fila == fila1) {
+                cout << "Ingrese el numero de productos que desea ingresar: "; cin >> num;
+
+                for (size_t i = 0; i < num; i++)
+                {
+                    cout << "Ingrese un texto: "; cin >> n;
+                    insertarlista(lista, n);
+                }
+            }
+            else if (fila == fila2) {
+
+            }
+            else if (fila == fila3) {
+
+            }
+            else if (fila == fila4) {
+
+            }
+            else
+            {
+                cout << "Esta fila no esta registrada";
+            }
+            
+            break;
+        case 2:
+           
+            break;
+        case 3:
+            break;
+        default:
+            break;
+        }
+   
 }
 void insertarlista(nodo*& lista, string n) {
     nodo* newnodo = new nodo();
@@ -26,6 +65,21 @@ void insertarlista(nodo*& lista, string n) {
 
     nodo *aux1 = lista;
     nodo* aux2;
-    
+   
+    while (aux1 != NULL && aux1->dato < n)
+    {
+        aux2 = aux1;
+        aux1 = aux1->next;
+    }
+    if (lista == aux1) {
+        lista = newnodo;
+    }
+    else
+    {
+        aux2->next = newnodo;
+    }
     newnodo->next = aux1;
+}
+void mostrarlista(nodo*& lista, string n) {
+
 }
