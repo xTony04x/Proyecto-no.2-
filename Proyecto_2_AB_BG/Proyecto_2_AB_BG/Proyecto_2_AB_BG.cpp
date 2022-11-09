e58891319e821b4c4429cde30eb259d17719e221
xTony0x <jabelteton@correo.utl.edu.gt>
2022-11-08 21:11:57 -0600

    creacion de cadenas

--- Proyecto_2_AB_BG/Proyecto_2_AB_BG/Proyecto_2_AB_BG.cpp
+++ Proyecto_2_AB_BG/Proyecto_2_AB_BG/Proyecto_2_AB_BG.cpp
@@ -17,7 +17,11 @@ using namespace std;
 struct nodo1//A1,B1,C1
 {
     string dato;
-    nodo1* next;
+    nodo1* next;  
+};
+struct pila
+{
+    const int maximo_pila = 5;
 };
 struct nodo2//A2,B2,C2
 {
@@ -34,8 +38,7 @@ struct nodo4//A4,B4,C4
     string dato;
     nodo4* next;
 };
-
-void insertarlista(nodo1*&, string);// FILA 1
+void insertarlista(nodo1*&, string[]);// FILA 1
 
 void insertarlista2(nodo2*&, string);// FILA 2
 
@@ -45,10 +48,19 @@ void insertarlista4(nodo4*&, string);// FILA 4
 
 void buscarlista(nodo1*, string);
 
+void inventario1(nodo1*);
+
 //MAIN - SE EJECUTARA TODO EL CODIGO
 int main()
 {
-    string n,n1;
+    
+    int peso, fecha, cantidad;
+    string n,n1,personaingreso;
+
+    string info[1];
+
+     info[1] = n, cantidad, peso;
+
     int num,menu,fila;
     int fila1 = 1, fila2 = 2 , fila3 = 3, fila4 = 4;
     nodo1* lista = NULL;
@@ -58,7 +70,7 @@ int main()
     
 inicio1:
     
-    cout << "Bodegas Inteligentes de Guatemala S.A. \n MENU : \n (1)-Almacenar Producto. \n (2)-Retirar Producto \n (3)-Mostrar productos \n "; cin >> menu;
+    cout << "\nBodegas Inteligentes de Guatemala S.A. \n MENU : \n (1)-Almacenar Producto. \n (2)-Retirar Producto \n (3)-Mostrar productos \n "; cin >> menu;
 
         switch(menu)
         {
@@ -66,17 +78,20 @@ inicio1:
           
             cout << "\n En que columna desea ingresar los productos? \n (1)(2)(3)(4)\n"; cin >> fila;
             if (fila == fila1) {
-                cout << "Ingrese el numero de productos que desea ingresar (MAX 3 productos por columna): "; cin >> num;
+                system("CLS");
 
-                for (size_t i = 0; i < num; i++)
-                {
-                    cout << "Ingrese un texto: "; cin >> n;
-                    insertarlista(lista, n);
-                }
+                    cout << "Ingrese un producto: "; cin >> n;
+                    cout << "Ingrese el peso del producto: "; cin >> peso;
+                    cout << "Ingrese la cantidad: "; cin >> cantidad;
+                    cout << "Ingrese la fecha de ingreso: "; cin >> fecha;
+                    cout << "¿Quien lo ingreso?: "; cin >>personaingreso ;
+                    insertarlista(lista, info);
                 
+              
                 goto inicio1;
             }
             else if (fila == fila2) {
+                system("CLS");
                 cout << "Ingrese el numero de productos que desea ingresar (MAX 3 productos por columna): "; cin >> num;
 
                 for (size_t i = 0; i < num; i++)
@@ -84,10 +99,11 @@ inicio1:
                     cout << "Ingrese un texto: "; cin >> n;
                     insertarlista2(lista2, n);
                 }
-
+                
                 goto inicio1;
             }
             else if (fila == fila3) {
+                system("CLS");
                 cout << "Ingrese el numero de productos que desea ingresar (MAX 3 productos por columna): "; cin >> num;
 
                 for (size_t i = 0; i < num; i++)
@@ -95,10 +111,11 @@ inicio1:
                     cout << "Ingrese un texto: "; cin >> n;
                     insertarlista3(lista3, n);
                 }
-
+                
                 goto inicio1;
             }
             else if (fila == fila4) {
+                system("CLS");
                 cout << "Ingrese el numero de productos que desea ingresar (MAX 3 productos por columna): "; cin >> num;
 
                 for (size_t i = 0; i < num; i++)
@@ -117,28 +134,31 @@ inicio1:
             
             break;
         case 2:
-            
+            system("CLS");
             cout << "Ingrese el producto que desea buscar: "; cin >> n1;
             buscarlista(lista, n1);
+            
             goto inicio1;
            
             break;
         case 3:
-
+            system("CLS");
+            inventario1(lista);
+            goto inicio1;
             break;
         default:
             break;
         }
    
 }
-void insertarlista(nodo1*& lista, string n) {
+void insertarlista(nodo1*& lista, string info[]) {
     nodo1* newnodo = new nodo1();
-    newnodo->dato = n;
+    newnodo->dato = info[1];
 
     nodo1 *aux1 = lista;
     nodo1* aux2;
    
-    while (aux1 != NULL && aux1->dato < n)
+    while (aux1 != NULL)
     {
         aux2 = aux1;
         aux1 = aux1->next;
@@ -153,25 +173,44 @@ void insertarlista(nodo1*& lista, string n) {
     newnodo->next = aux1;
 }
 
+//----------------arreglar----------------------------------------------------------------------
 void buscarlista(nodo1 *lista, string n1) {
     bool band = false;
     nodo1* actual = new nodo1();
     actual = lista;
   
-    while (actual != NULL && actual->dato == n1)
+    while (band != true)
     {
         if (actual->dato == n1) {
             band = true;
         }
-        actual = actual->next;
+        else
+        {
+
+            actual = actual->next;
+        }
+        
     }
   
     if (band == true) {
         cout << "\n se encontro el siguiente producto: " << n1 << "\n";
     }
-    
-   
-    
+    else
+    {
+        cout << "\nno esta registrado\n";
+    }
+}
+// ---------------------------------------------------------------------------------------------
+void inventario1(nodo1* lista1)
+{
+    nodo1* actual = new nodo1();
+
+    actual = lista1;
+    while (actual != NULL)
+    {
+        cout << "Producto: " << actual->dato << "\n";
+        actual = actual->next;
+    }
 }
 
 void insertarlista2(nodo2*& lista, string n) {
